ec2e1ffab8d4d1bf9058034ac8ad7c121561c6e7
Arief <ariefmaulanaa19@gmail.com>
2021-12-02 11:48:47 +0700

    [ARIEF] Create Layout Mobile Rest Garden

--- app/src/main/java/com/enigma/graveapplication/BookingListFragment.kt
+++ app/src/main/java/com/enigma/graveapplication/BookingListFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class LoginFragment : Fragment() {
+class BookingListFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_booking_list, container, false)
     }
 
 
--- app/src/main/java/com/enigma/graveapplication/TransactionFragment.kt
+++ app/src/main/java/com/enigma/graveapplication/TransactionFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class LoginFragment : Fragment() {
+class TransactionFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_transaction, container, false)
     }
 
 
